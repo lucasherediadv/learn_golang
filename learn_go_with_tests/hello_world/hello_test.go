46d3360dd6fdd68eea83db6bb8920bfb26331df6
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-03-20 15:48:52 -0300

    hello.go - hello_test.go

--- learn_go_with_tests/hello_world/hello_test.go
+++ learn_go_with_tests/hello_world/hello_test.go
@@ -7,13 +7,29 @@ import "testing"
 // Test function must start with the word "Test"
 // The test function takes one argument only "t *testing.T"
 func TestHello(t *testing.T) {
-    got := Hello("Lucas")
-    want := "Hello, Lucas"
+    // Subtests
+    // Sometimes it is useful to group tests around a "thing" and then
+    // have subtests describing different scenarios
+    t.Run("saying hello to people", func(t *testing.T) {
+        got := Hello("Lucas")
+        want := "Hello, Lucas"
+        assertCorrectMessage(t, got, want)
+    })
 
+    t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+        got := Hello("")
+        want := "Hello, World"
+        assertCorrectMessage(t, got , want)
+    })
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+    // Is needed to tell the test suite that this method is a helper.
+    // By doing this when it fails the line number reported will be in our
+    // function call rather than inside our test helper.
+    // This will help other developers track down problems easier
+    t.Helper()
     if got != want {
-        // Print out a message and fail the test
-        // "f" stands for format which allow us to build a string with values
-        // inserted into the placeholder values %q
         t.Errorf("got %q want %q", got, want)
     }
 }
